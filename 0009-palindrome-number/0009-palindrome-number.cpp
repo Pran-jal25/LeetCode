aba614b66844bf0de4c1cd64f3ac0b765286369b
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-08 14:25:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.2%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers are not palindrome
+        if(x < 0)
+            return false;
+
+        int original = x;
+        long long rev = 0;
+
+        // Reverse the number
+        while(x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        // Check if original and reversed are same
+        return original == rev;
+    }
+};
