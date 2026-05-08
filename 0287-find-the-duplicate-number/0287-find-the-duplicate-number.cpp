8dc8eb2eaed3128479c4185833bfc6bc795e7560
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-08 14:28:05 +0530

    Time: 5 ms (54.11%), Space: 65.1 MB (40.66%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+     
+    int slow=nums[0];
+    int fast=nums[0];
+
+    //moving pointers;
+    while(true)
+    {
+        slow=nums[slow]; //move 1 step;
+        fast=nums[fast]; //move 2 steps
+        fast=nums[fast]; //move 2 steps
+
+        if (slow==fast) break;
+    } 
+    //  find duplicate ie. starting point of linked list;
+    slow=nums[0];
+    while(slow!=fast)
+    {
+        slow=nums[slow]; //move 1 step;
+        fast=nums[fast];  //move 1 step;
+    }
+    return slow;
+    }
+};
