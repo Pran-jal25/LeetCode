c17a37635917e74af043899a205d0ecd2ca96302
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-10 01:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (5.10%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& arr) {
+        int n=arr.size();
+
+        int ans=arr[0];
+        int bestending=arr[0];
+
+        for(int i=1;i<n;i++) //arr[0] to already h so arr[1] se chalu kro
+        {
+            int v1=bestending+arr[i]; //pehle wala + abhi wala index;
+            int v2=arr[i]; //abhi jispe h "i" wo wala index;
+
+            bestending=max(v1,v2);//dono me se bada ans jiska hoga
+            ans=max(ans,bestending);
+        }
+        return ans;
+    }
+};
