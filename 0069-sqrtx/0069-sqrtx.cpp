72665406404d3b7ca4af0ef72e6628433de18382
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-12 16:07:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.28%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<=1)
+        return x;
+        
+        int start=0,end=x,mid,ans;
+
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+
+            // if(mid*mid==x) //instead mid==x/mid for stopping error;
+            if(mid==x/mid)
+            return mid;
+
+            else if(mid<x/mid)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else
+            end=mid-1;
+        }
+        return ans;  
+    }
+};
