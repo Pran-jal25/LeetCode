5a2708dea1b49a554aec883e80fc20cf99aea0b1
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-15 02:15:34 +0530

    Time: 4 ms (66.36%), Space: 24 MB (23.02%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>res;
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+
+        for(int i=1;i<n;i++)
+        {
+            int s=intervals[i][0];
+            int e=intervals[i][1];
+
+            if(end>=s) //overlapping condition so (merge hoga);
+            {
+                end=max(end,e);
+                continue;
+            } 
+    //if overlapping (merge) nhi hoga then simply push "i"th term in res;
+            res.push_back({start,end});
+            start=s;
+            end=e;
+        }
+        res.push_back({start,end});
+        return res;
+
+    }
+};
