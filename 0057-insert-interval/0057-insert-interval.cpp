e516e356a8e29d5d87b89bc21403ff0c7f37761c
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-16 01:52:04 +0530

    Time: 4 ms (16.54%), Space: 21.5 MB (93.97%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        intervals.push_back(newInterval);
+
+        sort(intervals.begin(), intervals.end());
+
+         vector<vector<int>>res;
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+
+        for(int i=1;i<n;i++)
+        {
+            int s=intervals[i][0];
+            int e=intervals[i][1];
+
+            if(end>=s) //overlapping condition so (merge hoga);
+            {
+                start=start;
+                end=max(end,e);
+                continue;
+            } 
+    //if overlapping (merge) nhi hoga then simply push "i"th term in res;
+            res.push_back({start,end});
+            start=s;
+            end=e;
+        }
+        res.push_back({start,end});
+        return res;
+    }
+};
