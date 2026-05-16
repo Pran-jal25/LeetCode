ac780ae28965e748e939f0faaaf11cc1484eee0f
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-17 00:54:44 +0530

    Time: 0 ms (100%), Space: 28.5 MB (5.14%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        //USING STACK;
+        stack<char>st;
+        vector<char>res;
+        int n=s.size();
+
+        for(int i=0;i<n;i++)
+        {
+            st.push(s[i]); //push the given string into stack;
+        }
+        //now see the "top" element and store it in a res vector;
+        while(!st.empty())
+        {
+            char c=st.top(); //see
+            st.pop();  //see and delete;
+            res.push_back(c);
+        }
+        // return res; it is a void fun so nothing is returned;
+
+        //copy the reverse characters back into "s";
+        for(int i=0;i<n;i++)
+        {
+            s[i]=res[i];
+        }
+    }
+};
