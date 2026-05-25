d7eaa70027bff870de19c55a29da40e153cc3f98
Pranjal <pranjalchoubeyxc@gmail.com>
2026-05-25 20:00:00 +0530

    Fix comment formatting in isPalindrome function

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers are not palindrome
+        //Negative numbers are not palindrome
         if(x < 0)
             return false;
 
