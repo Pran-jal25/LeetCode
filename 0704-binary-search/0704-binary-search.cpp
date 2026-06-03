fe85b49c5415d7e13e4d42983768a78d900a0b46
Pranjal <pranjalchoubeyxc@gmail.com>
2026-06-03 15:48:38 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.21%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int key) 
+{
+    int n=arr.size();
+    // start,end,mid;
+    int start=0,end=n-1,mid;
+
+    while(start<=end)
+    {
+     
+    // finding mid;
+    
+    // mid=(start+end)/2 ##this will give int flow error so use this instead;
+    mid=start + (end-start)/2;
+
+    if(arr[mid]==key)
+    return mid;
+
+    else if(arr[mid]<=key)
+    start=mid+1;
+
+    else
+    end=mid-1;
+    }
+    return -1;
+}
+};
