3e6fc35c388227ea8f9d08f1ada890d3a79b3be1
Pranjal <pranjalchoubeyxc@gmail.com>
2026-06-07 15:31:57 +0530

    Time: 0 ms (100%), Space: 9 MB (94.81%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        
+        vector<int> lower(26,0);//initialize with 0.
+        vector<int> upper(26,0);
+
+        //frequency update;
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]>='a')
+            lower[s[i]-'a']++;
+
+            else
+            upper[s[i]-'A']++;
+        }
+
+        int count=0; //total string size ie=ans h ye
+        bool odd=0; //odd ke count rkhega ye last me ye add krdnege agr  hoga to.
+
+        //odd even dekhkr count me add krenge;
+
+        //for lower
+        for(int i=0;i<26;i++)
+        {
+            if(lower[i]%2==0)
+            count+=lower[i]; //agr even h to direct add krdo
+
+            else
+            {count+=lower[i]-1; //agr odd h to ek minus krke even bnado fir add krdo
+            odd=1;
+            }
+
+        // for upper
+         if(upper[i]%2==0)
+            count+=upper[i]; //agr even h to direct add krdo
+
+            else
+            {count+=upper[i]-1; //agr odd h to ek minus krke even bnado fir add krdo
+            odd=1;
+            }
+        }
+        return count+odd;
+    }
+};
