f5612e5d6f044c9025f60f83336ed1b795177885
Pranjal <pranjalchoubeyxc@gmail.com>
2026-06-20 16:00:53 +0530

    Time: 0 ms (100%), Space: 9.2 MB (58.01%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string result = "";
+
+        while (i >= 0 || j >= 0 || carry) {
+            int d1 = (i >= 0) ? (num1[i--] - '0') : 0;
+            int d2 = (j >= 0) ? (num2[j--] - '0') : 0;
+
+            int total = d1 + d2 + carry;
+            carry = total / 10;
+            result += (char)('0' + total % 10);
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
