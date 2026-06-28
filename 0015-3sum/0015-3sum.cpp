ff1051efeaff18371aa2c27e5cc29d5934ca62a0
Pranjal <pranjalchoubeyxc@gmail.com>
2026-06-28 15:56:19 +0530

    Update 0015-3sum.cpp

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
         vector<vector<int>> res;
         int n=nums.size();
         sort(nums.begin(),nums.end()); //sort first;
