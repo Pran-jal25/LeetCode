14af3986ead91375f44afc02e73eedc09510b54c
Pranjal <pranjalchoubeyxc@gmail.com>
2026-06-28 15:55:29 +0530

    Fix comment typo in threeSum function

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        //code
-
+        //cod
         vector<vector<int>> res;
         int n=nums.size();
         sort(nums.begin(),nums.end()); //sort first;
