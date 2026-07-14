44b1d7d1dc5752af0633457b4a5572245f0403e1
Pranjal <pranjalchoubeyxc@gmail.com>
2026-07-14 14:34:32 +0530

    Update 0056-merge-intervals.cpp

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //code her
+        //code he
         vector<vector<int>>res;
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
