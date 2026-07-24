b91075908035986c0cdef4edb65e6ad88e34abfa
Pranjal <pranjalchoubeyxc@gmail.com>
2026-07-24 17:41:53 +0530

    Modify comment in searchMatrix method
    Updated comment in searchMatrix function.

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        //code he
+        //code h
         
         int n=matrix.size();
         int m=matrix[0].size(); //m= columns
