7c3fd9079085b92401ca982360756fb38f5667cb
Pranjal <pranjalchoubeyxc@gmail.com>
2026-08-17 23:53:46 +0530

    Correct typo in merge function comment
    Fixed a typo in the comment for the merge function.

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //code here
+        //code her
         vector<vector<int>>res;
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
