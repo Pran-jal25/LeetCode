4d6665e7810772f20e6480c927512f56c9588b91
Pranjal <pranjalchoubeyxc@gmail.com>
2026-09-21 00:50:18 +0530

    Time: 1467 ms (35.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select p.product_name as product_name,s.year as year,s.price as price
+from Sales s
+left join Product p
+on p.product_id=s.product_id; #yahi dono bs colums same h dono  table me isliye on basis of these two
