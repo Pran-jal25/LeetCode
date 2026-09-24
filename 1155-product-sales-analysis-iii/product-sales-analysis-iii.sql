ea9130dec5f083bcbac8dd07d3b99842f9006fb4
Pranjal <pranjalchoubeyxc@gmail.com>
2026-09-24 23:57:17 +0530

    Time: 680 ms (93.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+select
+    product_id,
+    year as first_year,
+    quantity,
+    price
+from Sales
+where(product_id,year) IN(
+-- Find the first year for every product (subquery): 
+select 
+    product_id,
+    MIN(year)
+from Sales
+group by product_id
+);
